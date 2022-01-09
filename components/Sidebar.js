ae9f34eb97b9c047178452e94b466d25838226d1
Mogakamo <mogaka.amo254@gmail.com>
2022-01-09 16:08:19 +0300

    Sidebar is taking to shape

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
+import {
+  HomeIcon,
+  LibraryIcon,
+  SearchIcon,
+  PlusCircleIcon,
+  HeartIcon,
+  RssIcon,
+} from "@heroicons/react/outline";
 
 function Sidebar() {
-    return (
-        <div>
-            <h1>I am a sidebar</h1>
-        </div>
-    )
+  return (
+    <div className="text-gray-500 p-5 sm:text-sm border-r border-gray-900">
+      <div className="space-y-4">
+        <button className="flex p-2 items-center space-x-2 hover:text-white">
+          <HomeIcon className="h-5 w-5" />
+          <p>Home</p>
+        </button>
+        <button className="flex p-2 items-center space-x-2 hover:text-white">
+          <SearchIcon className="h-5 w-5" />
+          <p>Search</p>
+        </button>
+        <button className="flex p-2 items-center space-x-2 hover:text-white">
+          <LibraryIcon className="h-5 w-5" />
+          <p>Your Library</p>
+        </button>
+
+        <hr className="border-t-{0.1px} border-gray-900" />
+
+        <button className="flex p-2 items-center space-x-2 hover:text-white">
+          <PlusCircleIcon className="h-5 w-5" />
+          <p>Your Library</p>
+        </button>
+        <button className="flex p-2 items-center space-x-2 hover:text-white">
+          <HeartIcon className="h-5 w-5" />
+          <p>Liked Songs</p>
+        </button>
+        <button className="flex p-2 items-center space-x-2 hover:text-white">
+          <RssIcon className="h-5 w-5" />
+          <p>Your episodes</p>
+        </button>
+        <hr className="border-t-{0.1px} border-gray-900" />
+
+        {/* Playlists */}
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
