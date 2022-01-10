571924190620fb487d2ea71d997f7bfb78da93eb
Mogakamo <mogaka.amo254@gmail.com>
2022-01-10 05:30:51 +0300

    commit

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -13,7 +13,7 @@ function Sidebar() {
   const { data: session, status } = useSession();
 
   return (
-    <div className="text-gray-500 p-5 sm:text-sm border-r border-gray-900 overf">
+    <div className="text-gray-500 p-5 sm:text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen">
       <div className="space-y-4">
         <button
           className="flex p-2 items-center space-x-2 hover:text-white"
