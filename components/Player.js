e4a1b2cd2e3bcda5a49f1e76d771fe9d492d1f9c
Mogakamo <mogaka.amo254@gmail.com>
2022-01-11 14:14:05 +0300

    commit

--- components/Player.js
+++ components/Player.js
@@ -1,13 +1,19 @@
 import {
-  
+  HeartIcon,
+  VolumeUpIcon as VolumeDownIcon,
   SwitchHorizontalIcon,
 } from "@heroicons/react/outline";
-import {RewindIcon, PauseIcon,
-    PlayIcon,
-    FastForwardIcon,
-    ReplyIcon,} from "@heroicons/react/solid"
+import {
+  RewindIcon,
+  PauseIcon,
+  PlayIcon,
+  FastForwardIcon,
+  ReplyIcon,
+  VolumeUpIcon,
+} from "@heroicons/react/solid";
+import { debounce } from "lodash";
 import { useSession } from "next-auth/react";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
 import useSpotify from "../hooks/useSpotify";
@@ -46,16 +52,29 @@ function Player() {
   }, [currentTrackId, spotifyApi, session]);
 
   const handlePlayPause = () => {
-      spotifyApi.getMyCurrentPlaybackState().then((data) => {
-          if (spotifyApi.getAccessToken() && data.body.is_playing) {
-              spotifyApi.pause()
-              setIsPlaying(false)
-          } else {
-              spotifyApi.play()
-              setIsPlaying(true)
-          }
-      })
-  }
+    spotifyApi.getMyCurrentPlaybackState().then((data) => {
+      if (spotifyApi.getAccessToken() && data.body.is_playing) {
+        spotifyApi.pause();
+        setIsPlaying(false);
+      } else {
+        spotifyApi.play();
+        setIsPlaying(true);
+      }
+    });
+  };
+
+  useEffect(() => {
+    if (volume > 0 && volume < 100) {
+      debounceAdjustVolume(volume);
+    }
+  }, [volume]);
+
+  const debounceAdjustVolume = useCallback(
+    debounce((volume) => {
+      spotifyApi.setVolume(volume).catch((err) => console.log(err));
+    }, 50),
+    []
+  );
 
   return (
     <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
@@ -74,16 +93,33 @@ function Player() {
       {/* Center */}
       <div className="flex items-center justify-evenly">
         <SwitchHorizontalIcon className="button" />
-        <RewindIcon className="button" 
-        />
+        <RewindIcon className="button" />
         {isPlaying ? (
           <PauseIcon onClick={handlePlayPause} className="button w-10 h-10" />
         ) : (
           <PlayIcon onClick={handlePlayPause} className="button w-10 h-10" />
         )}
-        <FastForwardIcon className="button" 
-        />
+        <FastForwardIcon className="button" />
         <ReplyIcon className="button" onClick={spotifyApi.setRepeat()} />
+      </div>
+      {/* Right */}
+      <div className="flex items-center space-x-3 md:space-x-4 justify-end py-5">
+        <VolumeDownIcon
+          onClick={() => volume > 0 && setVolume(volume - 10)}
+          className="button"
+        />
+        <input
+          type="range"
+          className="w-14 md:w-28"
+          value={volume}
+          onChange={(e) => setVolume(Number(e.target.value))}
+          min={4}
+          max={100}
+        />
+        <VolumeUpIcon
+          onClick={() => volume < 100 && setVolume(volume + 10)}
+          className="button"
+        />
       </div>
     </div>
   );
@@ -91,6 +127,5 @@ function Player() {
 
 export default Player;
 
-
-        // onClick={() => spotifyApi.skipToPrevious()} 
-        // onClick={() => spotifyApi.skipToNext()}
+// onClick={() => spotifyApi.skipToPrevious()}
+// onClick={() => spotifyApi.skipToNext()}
