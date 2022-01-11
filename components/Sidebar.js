0241efaae3477651c4bde73fbbdd489fd06bcd39
Mogakamo <mogaka.amo254@gmail.com>
2022-01-11 08:02:13 +0300

    finished with the ui... Now working on playing a song

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -31,15 +31,11 @@ function Sidebar() {
   console.log(playlists);
 
   return (
-    <div className="text-gray-500 p-5 sm:text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide 
-    h-screen text-xs lg:text-sm sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex">
+    <div
+      className="text-gray-500 p-5 sm:text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide 
+    h-screen text-xs lg:text-sm sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex"
+    >
       <div className="space-y-4">
-        <button
-          className="flex p-2 items-center space-x-2 hover:text-white"
-          onClick={() => signOut()}
-        >
-          <p>Logout</p>
-        </button>
         <button className="flex p-2 items-center space-x-2 hover:text-white">
           <HomeIcon className="h-5 w-5" />
           <p>Home</p>
@@ -71,7 +67,13 @@ function Sidebar() {
 
         {playlists.map((playlist) => {
           return (
-            <p key={playlist.id} onClick={() => {setPlaylistId(playlist.id)}} className="cursor-pointer hover:text-white">
+            <p
+              key={playlist.id}
+              onClick={() => {
+                setPlaylistId(playlist.id);
+              }}
+              className="cursor-pointer hover:text-white"
+            >
               {playlist.name}
             </p>
           );
--- components/Song.js
+++ components/Song.js
@@ -1,11 +1,12 @@
 import React from "react";
 import useSpotify from "../hooks/useSpotify";
+import { millisToMinutesAndSeconds } from "../lib/time";
 
 function Song({ order, track }) {
   const spotifyApi = useSpotify();
   return (
-    <div className="grid grid-cols-2">
-      <div className="flex items-center space-x-4">
+    <div className="grid grid-cols-2 text-gray-500 cursor-pointer px-5 py-4 rounded-lg hover:bg-gray-900">
+      <div className="flex items-center space-x-4 ">
         <p>{order + 1}</p>
         <img
           className="h-10 w-10"
@@ -14,13 +15,13 @@ function Song({ order, track }) {
         />
 
         <div>
-          <p>{track.track.name}</p>
-          <p>{track.track.artists[0].name}</p>
+          <p className="w-36 md:w-64 text-white truncate">{track.track.name}</p>
+          <p className="w-40">{track.track.artists[0].name}</p>
         </div>
       </div>
       <div className="items-center flex ml-auto md:ml-0 justify-between">
-        <p className="hidden md:inline">{track.track.album.name}</p>
-        <p>duration</p>
+        <p className="w-40 hidden md:inline">{track.track.album.name}</p>
+        <p>{millisToMinutesAndSeconds(track.track.duration_ms)}</p>
       </div>
     </div>
   );
